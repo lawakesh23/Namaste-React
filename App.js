e6cfab509178f957fb046a276b09e8ac925810bb
unknown <lawakesh23@gmail.com>
2023-01-12 21:08:17 +0530

    Pushing Chapter 3

--- App.js
+++ App.js
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 
-const heading = React.createElement('h1', {}, 'Namaste React');
-const heading2 =React.createElement('h2',{}, 'Namaste React 2');
-const container = React.createElement('div',{ id: 'container'}, [heading, heading2]);
+const Title = ()=>(
+      <h1 className="MyClass" id="main"> Namaste React</h1>
+);
+
+const HeaderComponent =()=>{
+     return(
+        <div>
+            <Title/>
+            <h1 className="my-heading">This is heading</h1>
+        </div>
+     );
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(container);
+root.render(<HeaderComponent/>);
